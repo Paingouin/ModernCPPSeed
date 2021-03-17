923420e3ccf669537c51e0c18df0e9ed99be0fbd
Paingouin <jordan.bussiere21@gmail.com>
2021-03-17 15:43:40 +0100

    Clean cmake command

--- Tools/Builders/build.bat
+++ Tools/Builders/build.bat
@@ -13,7 +13,7 @@ cd ../..
 mkdir Build
 cd Build
 
-cmake .. -G"Visual Studio 15 2017" -Ax64  -DCMAKE_INSTALL_PREFIX=../install
+cmake -S . -G"Visual Studio 15 2017" -Ax64  -DCMAKE_INSTALL_PREFIX=../install
 
 rem package phase
 REM cmake --build . --target Package --config Debug

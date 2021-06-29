21e57c46ac7a75800e24bb22de60030b3c3bdf10
Paingouin <jordan.bussiere21@gmail.com>
2021-06-29 06:34:35 +0200

    Forgot to remove bincrafter references

--- CMake/ConanConfig/conanfile.py
+++ CMake/ConanConfig/conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile, CMake
 
 class RequirementConan(ConanFile):
    settings = "os", "compiler", "build_type", "arch"
-   requires = "sdl2/2.0.14@bincrafters/stable","freetype/2.10.0@bincrafters/stable","glew/2.1.0@bincrafters/stable","gtest/1.8.1@bincrafters/stable","glm/0.9.9.8" # comma-separated list of requirements
+   requires = "ssdl/2.0.14","freetype/2.10.0","glew/2.2.0","gtest/1.8.1","glm/0.9.9.8" # comma-separated list of requirements
    generators = "cmake_multi"
    default_options = {"sdl2:shared": True, "glew:shared" : False, "gtest:build_gmock": True, "gtest:shared" :False}
 
